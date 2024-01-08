a9c97e2dccef0d2cf4fe79b8203874d46333c5c2
Nagesh Rathod <111345802+nageshredhat@users.noreply.github.com>
2024-01-08 16:40:42 +0530

    sample node add

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const webpack = require('webpack');
-
-module.exports = {
-  devServer: {
-    port: 3000,
-    host: '0.0.0.0', // This will allow requests from all hosts
-    historyApiFallback: true,
-  },
-};
